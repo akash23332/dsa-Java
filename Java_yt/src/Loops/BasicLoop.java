64c7424342db3c947d5955e283625d54ee6e7d2d
Akash Sharma <akak9781189@gmail.com>
2026-02-16 17:22:34 +0530

    Added loops

---
+++ Java_yt/src/Loops/BasicLoop.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class BasicLoop {
+    public static void main(String[] args) {
+//        for (int i = 7; i <=12; i++) {
+//            System.out.println("Amir Hates Nikita");
+//        }
+        int i;
+        for ( i = 1; i <=8; i++) {
+            System.out.println(i+" ");
+        }
+
+    }
+
+}
