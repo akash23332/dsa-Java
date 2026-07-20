c868a366a03973e86c6e6186fa011d8a02b81456
Akash Sharma <akak9781189@gmail.com>
2026-07-20 20:55:08 +0530

    Recursion

---
+++ Java_yt/src/Recursion/PowerOfHanoi.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class PowerOfHanoi 
+{
+    public static void main(String[] args) {
+
+//        hanoi(3,'A','B','C');
+
+        int a=070;
+        System.out.println(a);
+
+
+
+//    private static void hanoi(int n,char curr,char via ,char dest) {
+//        if(n==0) return;
+//        hanoi(n-1,curr,dest,via);
+//        System.out.println(curr+"-"+dest);
+//        hanoi(n-1, via, curr, dest);
+
+
+    }
+}
